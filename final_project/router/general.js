2d4f6de6bd9b1c15a7dc5223a2979753d6ea8352
Dario <dario.pratola@gmail.com>
2023-12-03 10:29:36 -0500

    task5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,58 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books,null,4)); 
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const ISBNToFind = req.params.isbn;
+    const booksByISBN= Object.values(books).filter(book=>book.ISBN===ISBNToFind);
+    if (booksByISBN.length>0){
+      res.send(booksByISBN);  
+    }else{
+        res.status(404).json({message: `Nessun libro trovato per ISBN ${ISBNToFind}`})
+    }
+    
+});
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const authorToFind = req.params.author;
+    
+    // Cerca tutti i libri scritti dall'autore specificato
+    const booksByAuthor = Object.values(books).filter(book => book.author === authorToFind);
+
+    if (booksByAuthor.length > 0) {
+        res.send(booksByAuthor);
+    } else {
+        res.status(404).json({ message: `Nessun libro trovato per l'autore ${authorToFind}` });
+    }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const titleToFind=req.params.title;
+  const booksByTitle=Object.values(books).filter(book=>book.title===titleToFind);
+  if (booksByTitle.length>0){
+      res.send(booksByTitle);
+  } else{
+    res.status(404).json({ message: `Nessun libro trovato per titolo ${authorToFind}` });
+}
+
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const ISBNToFind = req.params.isbn;
+    const booksByISBN = Object.values(books).find(book => book.ISBN === ISBNToFind);
+    if (booksByISBN) {
+        const reviews = booksByISBN.reviews || {};
+        res.send(reviews);
+    } else {
+        res.status(404).json({ message: `Nessun libro trovato per ISBN ${ISBNToFind}` });
+    }
 });
 
 module.exports.general = public_users;

4bde9f94cdbd78484cbff3912525187b088c6b5b
Dario <dario.pratola@gmail.com>
2023-12-03 14:21:37 -0500

    completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,7 +27,7 @@ public_users.get('/',function (req, res) {
 });
 
 // Get the book list using async-await
-public_users.get('/all', async function (req, res) {
+public_users.get('/async', async function (req, res) {
     try {
         const bookList = books;
         res.send(JSON.stringify(bookList, null, 4));
@@ -50,6 +50,24 @@ public_users.get('/isbn/:isbn', function (req, res) {
     
 });
 
+public_users.get('/isbn-async/:isbn', async function (req, res) {
+    try {
+        const ISBNToFind = req.params.isbn;
+        
+        // Simula una chiamata asincrona per ottenere i dettagli del libro dall'oggetto 'books'
+        const bookDetails = Object.values(books).find(book => book.ISBN === ISBNToFind);
+
+        if (bookDetails) {
+            res.send(bookDetails);
+        } else {
+            res.status(404).json({ message: `Nessun libro trovato per ISBN ${ISBNToFind}` });
+        }
+    } catch (error) {
+        console.error('Errore durante il recupero dei dettagli del libro:', error);
+        res.status(500).json({ message: 'Errore durante il recupero dei dettagli del libro' });
+    }
+});
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -65,6 +83,25 @@ public_users.get('/author/:author',function (req, res) {
     }
 });
 
+// Get book details based on author using async-await
+public_users.get('/author-async/:author', async function (req, res) {
+    try {
+        const authorToFind = req.params.author;
+
+        // Simula una chiamata asincrona per ottenere i libri scritti dall'autore specificato
+        const booksByAuthor = Object.values(books).filter(book => book.author === authorToFind);
+
+        if (booksByAuthor.length > 0) {
+            res.send(booksByAuthor);
+        } else {
+            res.status(404).json({ message: `Nessun libro trovato per l'autore ${authorToFind}` });
+        }
+    } catch (error) {
+        console.error('Errore durante il recupero dei libri dell\'autore:', error);
+        res.status(500).json({ message: 'Errore durante il recupero dei libri dell\'autore' });
+    }
+});
+
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   const titleToFind=req.params.title;
@@ -75,6 +112,25 @@ public_users.get('/title/:title',function (req, res) {
     res.status(404).json({ message: `Nessun libro trovato per titolo ${authorToFind}` });
 }
 
+});
+
+// Get book details based on title using async-await
+public_users.get('/title-async/:title', async function (req, res) {
+    try {
+        const titleToFind = req.params.title;
+
+        // Simula una chiamata asincrona per ottenere i libri con il titolo specificato
+        const booksByTitle = Object.values(books).filter(book => book.title === titleToFind);
+
+        if (booksByTitle.length > 0) {
+            res.send(booksByTitle);
+        } else {
+            res.status(404).json({ message: `Nessun libro trovato per il titolo ${titleToFind}` });
+        }
+    } catch (error) {
+        console.error('Errore durante il recupero dei libri per titolo:', error);
+        res.status(500).json({ message: 'Errore durante il recupero dei libri per titolo' });
+    }
 });
 
 //  Get book review
